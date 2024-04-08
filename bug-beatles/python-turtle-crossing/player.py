63257f5249cabc49c353e1d1ebb550a8b088db64
Siddharth Tiwari <tiwarisid022018@gmail.com>
2024-04-08 14:58:28 +0530

    added y coordinate and speed features

--- bug-beatles/python-turtle-crossing/player.py
+++ bug-beatles/python-turtle-crossing/player.py
@@ -3,8 +3,8 @@ from turtle import Turtle
 
 class Player(Turtle):
 
-    def __init__(self):
-        super().__init__()
+    def _init_(self):
+        super()._init_()
         self.penup()
         self.shape("turtle")
         self.goto(0, -270)
